5b8999a1bb645809cf132d2f805a87ca67b6dc48
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-06 19:18:52 +0300

    fix

--- utils/courses/english_tutor/utils_add_vk_files.js
+++ utils/courses/english_tutor/utils_add_vk_files.js
@@ -2,8 +2,8 @@ fs = require('fs');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-file_path = 'english_tutor_theory.json';
-file_suffix = 'TH';
+file_path = 'english_tutor_tasks.json';
+file_suffix = 'PR';
 console.log(file_path);
 
 var json = fs.readFileSync(file_path);
@@ -13,7 +13,7 @@ for (var key in json) {
         console.log(key, json[key]);
         for (var local_key in json[key]){
             if (json[key].hasOwnProperty(local_key)){
-                json[key][local_key] = {'vk_file': json[key][local_key][0], 'tg_file': json[key][local_key][1]}
+                json[key][local_key] = {'vk_file': json[key][local_key][0], 'tg_file': 'Unit_' + toString(parseInt(key) + 1) + '_' + file_suffix + '.pdf', 'answers': json[key][local_key][1]}
             }
         }
     }
