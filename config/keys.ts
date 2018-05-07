dccd3ccdc3718b815fc6aa9c5c3c2194a76b3c28
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-07 23:31:12 +0300

    fix

--- config/keys.ts
+++ config/keys.ts
@@ -2,3 +2,5 @@ export const vk_token = '251d5365d4fa3f8f44fa1a29a95fd8df030a094ebfa6b4d536cefbd
 export const vk_admin_id = 200234103;
 export const tg_token = '514641629:AAEF5RPoJmQ8N0WjKVakIIJWd7sz85cTIQc';
 export const tg_admin_id = 340473490;
+
+export const mongo_db_url = 'mongodb://fedor.a.hope:MGTA2019@ds217560.mlab.com:17560/apok';
