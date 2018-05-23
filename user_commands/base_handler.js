7727f88ba075fd7d92f492f0f02abb38fc3e1cd3
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-23 22:54:42 +0300

    fix

--- user_commands/base_handler.js
+++ user_commands/base_handler.js
@@ -1,8 +1,11 @@
+const save_help = require('./utils/save_help');
+
 const  baseHandler = function(id, text, platform){
     switch (text){
         case /\/start*./:
             break;
         case /\/help*./:
+            save_help(id, text, platform);
             break
         case /\/getclasswork*./:
             break
