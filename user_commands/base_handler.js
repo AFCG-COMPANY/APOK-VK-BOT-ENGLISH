7e911e2c50b1246503f35edcbacf6ec1ba07dd9e
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-23 23:32:55 +0300

    fix

--- user_commands/base_handler.js
+++ user_commands/base_handler.js
@@ -1,20 +1,27 @@
-const save_help = require('./utils/save_help');
+'use strict';
 
-const  baseHandler = function(id, text, platform){
-    switch (text){
-        case /\/start*./:
-            break;
-        case /\/help*./:
-            save_help(id, text, platform);
-            break
-        case /\/getclasswork*./:
-            break
-        case /\/gethomework*./:
-            break
-        case /\/sendhomework*./:
-            break
-        default:
-            break
+const
+    regexp_commands = require('./utils/regexp_commands');
+
+const
+    save_help = require('./utils/save_help');
+
+const
+    baseHandler = function(id, text, platform){
+        switch (text){
+            case regexp_commands.startRegexp:
+                break;
+            case regexp_commands.helpRegexp:
+                save_help(id, text, platform);
+                break
+            case regexp_commands.getclassworkRegexp:
+                break
+            case regexp_commands.gethomeworkRegexp:
+                break
+            case regexp_commands.sendhomeworkRegexp:
+                break
+            default:
+                break
+        }
     }
-}
 exports.baseHandler = baseHandler;
