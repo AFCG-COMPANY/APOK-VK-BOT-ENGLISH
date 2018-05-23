4818c97d4730f93875eb9024d44b8533f4091d94
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-23 23:23:38 +0300

    fix

---
+++ user_commands/utils/regexp_commands.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startRegexp = /\/start*./;
+exports.helpRegexp = /\/help*./;
+exports.getclassworkRegexp = /\/getclasswork*./;
+exports.gethomeworkRegexp = /\/gethomework*./;
+exports.sendhomeworkRegexp = /\/sendhomework*./;
