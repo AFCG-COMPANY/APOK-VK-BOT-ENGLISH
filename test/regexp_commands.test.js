d20a9eec886d2da8f1e50c063cc93ef65021b2d4
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-24 18:54:02 +0300

    fix

--- test/regexp_commands.test.js
+++ test/regexp_commands.test.js
@@ -9,6 +9,9 @@ describe('Check regexps', function() {
         it('(/start) must be true', function() {
             assert.equal(regexp_commands.startRegexp.test('/start'), true);
         });
+        it('(/start   ) must be true', function() {
+            assert.equal(regexp_commands.startRegexp.test('/start  '), true);
+        });
         it('(/start 213) must be true', function() {
             assert.equal(regexp_commands.startRegexp.test('/start 123'), true);
         });
@@ -18,6 +21,24 @@ describe('Check regexps', function() {
         });
         it('(/starsdf 123) must be false', function() {
             assert.equal(regexp_commands.startRegexp.test('/starsdf 123'), false);
+        });
+    });
+    describe('check help regexp', function() {
+        it('(/help) must be true', function() {
+            assert.equal(regexp_commands.helpRegexp.test('/help'), true);
+        });
+        it('(/help   ) must be true', function() {
+            assert.equal(regexp_commands.helpRegexp.test('/help   '), true);
+        });
+        it('(/help 213) must be true', function() {
+            assert.equal(regexp_commands.helpRegexp.test('/help 123'), true);
+        });
+
+        it('(/helsdf) must be false', function() {
+            assert.equal(regexp_commands.helpRegexp.test('/helsdf'), false);
+        });
+        it('(/helsdf 123) must be false', function() {
+            assert.equal(regexp_commands.helpRegexp.test('/helsdf 123'), false);
         });
     });
 });
