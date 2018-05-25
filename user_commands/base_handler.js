aa816ac7c4a7cefece183a38fd5c2bf5d8821d17
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-25 20:44:06 +0300

    fix

--- user_commands/base_handler.js
+++ user_commands/base_handler.js
@@ -4,30 +4,32 @@ const
     regexp_commands = require('./utils/regexp_commands');
 
 const
-    save_help = require('./utils/save_help');
+    start = require('./utils/start'),
+    save_help = require('./utils/save_help'),
+    getclasswork = require('./utils/getclasswork'),
+    gethomework = require('./utils/gethomework'),
+    sendhomework = require('./utils/sendhomework');
 
 const
     baseHandler = function(id, text, platform) {
-        console.log(id, text, platform);
         if (regexp_commands.startRegexp.test(text)) {
-            return;
+            return start(id, text, platform);
         }
 
         if (regexp_commands.helpRegexp.test(text)) {
             console.log('help');
-            save_help(id, text, platform);
-            return;
+            return save_help(id, text, platform);
         }
 
         if (regexp_commands.getclassworkRegexp.test(text)) {
-            return;
+            return getclasswork(id, text, platform);
         }
 
         if (regexp_commands.gethomeworkRegexp.test(text)) {
-            return;
+            return gethomework(id, text, platform);
         }
         if (regexp_commands.sendhomeworkRegexp.test(text)){
-           return;
+           return sendhomework(id, text, platform);
         }
 
     }
