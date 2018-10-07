e8169b93b6e7b3fbbc0693cf0b75cb2cdc7d71a4
Fedor Arbuzov <arbuzovfp@gmail.com>
2018-10-07 20:06:51 +0300

    add python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 import vk
+from telegram.ext import Updater, CommandHandler
 
 
 session = vk.Session(access_token='e4156c1e9a2000469195d054ba11bdafe5869b64e72cbef61dab1f5f00f96631cb710e664a49e8c4fa66f')
@@ -7,16 +8,18 @@ api = vk.API(session, oauth=1, v='5.80', lang='ru', timeout=10)
 print(api.messages.getConversations())
 print(api.messages.getHistory(user_id=200234103))
 api.messages.send(user_id=200234103, message='text')
-messages = api.messages
-last = messages['items'][0]['id']
 
-while True:
-    try:
-        messages = api.messages.get(last_message_id=last)
 
-    except Exception as e:
-        print('error')
-    sleep(2)
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
 
+
+updater = Updater('514641629:AAGyRyh5kvd1f59l9nQ4787-AGl7NOauyNg')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
 
 
